3a75fbf415e4c63cc8c2251d93ab7c010848d584
Eolien55 <elielevaillant2007@gmail.com>
2021-12-04 21:27:24 +0100

    Program can now rember 😄 codes

--- src/test.rs
+++ src/test.rs
@@ -18,11 +18,19 @@ fn test_expand() {
     .collect();
 
     assert_eq!(
-        run::expand(&"{{fr}".to_string(), &codes, '{', '}', None),
+        run::expand(
+            &"{{fr}".to_string(),
+            &codes,
+            '{',
+            '}',
+            None,
+            &mut vec![],
+            ','
+        ),
         "{French"
     );
     assert_eq!(
-        run::expand(&"{fr".to_string(), &codes, '{', '}', None),
+        run::expand(&"{fr".to_string(), &codes, '{', '}', None, &mut vec![], ','),
         "{fr"
     );
     assert_eq!(
@@ -31,7 +39,9 @@ fn test_expand() {
             &codes,
             '{',
             '}',
-            None
+            None,
+            &mut vec![],
+            ','
         ),
         "French History (18th Century)"
     );
@@ -41,30 +51,97 @@ fn test_expand() {
             &codes,
             '{',
             '}',
-            None
+            None,
+            &mut vec![],
+            ','
         ),
         "{fr History (18th Century)"
     );
 
     assert_eq!(
         run::expand(
-            &run::expand(&"[sh{1}]".to_string(), &codes, '{', '}', None),
+            &run::expand(
+                &"[sh{1}]".to_string(),
+                &codes,
+                '{',
+                '}',
+                None,
+                &mut vec![],
+                ','
+            ),
             &codes,
             '[',
             ']',
-            None
+            None,
+            &mut vec![],
+            ','
         ),
         "Shell One"
     );
 
     assert_eq!(
         run::expand(
-            &run::expand(&"{sh{2}}".to_string(), &codes, '{', '}', Some(3)),
+            &run::expand(
+                &"{sh{2}}".to_string(),
+                &codes,
+                '{',
+                '}',
+                Some(3),
+                &mut vec![],
+                ','
+            ),
             &codes,
             '{',
             '}',
-            None
+            None,
+            &mut vec![],
+            ','
         ),
         "Shell Two"
     );
+
+    assert_eq!(
+        run::expand(
+            &run::expand(
+                &"{,{2}}",
+                &codes,
+                '{',
+                '}',
+                None,
+                &mut vec!["sh".to_string()],
+                ','
+            ),
+            &codes,
+            '{',
+            '}',
+            None,
+            &mut vec!["sh".to_string()],
+            ','
+        ),
+        "Shell Two"
+    );
+}
+
+#[test]
+fn test_decode() {
+    use crate::run;
+
+    let history: Vec<_> = vec!["fr", "hst", "cnt", "1", "2", "shone", "shtwo"]
+        .iter()
+        .map(|x| x.to_string())
+        .collect();
+
+    assert_eq!(
+        run::expand_last(&",,".to_string(), &history, ','),
+        "History"
+    );
+    assert_eq!(
+        run::expand_last(&",,,,,,,,".to_string(), &history, ','),
+        ",,,,,,,,"
+    );
+    assert_eq!(
+        run::expand_last(&",aa".to_string(), &history, ','),
+        ",aa"
+    );
+    assert_eq!(run::expand_last(&"".to_string(), &history, ','), "");
 }
